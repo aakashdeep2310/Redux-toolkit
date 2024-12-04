923b88d7666dc23df7ee9a092264cd98009ba238
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-12-04 23:17:09 +0530

    3rd commit of the day

--- redux-toolkit/src/features/counter/Counter.jsx
+++ redux-toolkit/src/features/counter/Counter.jsx
@@ -1,46 +1,54 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment, reset } from '../counter/counterSlice'
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, reset, incrementByAmount } from "../counter/counterSlice";
+import { useState } from "react";
 export default function Counter() {
-  const count = useSelector((state) => state.counter.value) // manage the state
-  const dispatch = useDispatch()
 
-  function handleIncrementClick(){
-    dispatch(increment())
+  const [amount, setAmount] = useState(0);
+  const count = useSelector((state) => state.counter.value); // manage the state
+  const dispatch = useDispatch();
+
+  function handleIncrementClick() {
+    dispatch(increment());
   }
 
-  function handledecrementClick(){
-    dispatch(decrement())
+  function handledecrementClick() {
+    dispatch(decrement());
   }
-  function handleResetClick(){
-    dispatch(reset())
+  function handleResetClick() {
+    dispatch(reset());
+  }
+
+  function handleIncrementNumber(){
+    dispatch(incrementByAmount(amount))
   }
 
   return (
     <div>
       <div>
-        <button
-          aria-label="Increment value"
-          onClick={handleIncrementClick}
-        >
-          Increment + 
+        <button aria-label="Increment value" onClick={handleIncrementClick}>
+          Increment +
         </button>
         <span>{count}</span>
         <br />
-        <button
-          aria-label="Decrement value"
-          onClick={handledecrementClick}
-        >
+        <button aria-label="Decrement value" onClick={handledecrementClick}>
           Decrement -
         </button>
-        <button
-          aria-label="Reset value"
-          onClick={handleResetClick}
-        >
-          Reset 
+        <button aria-label="Reset value" onClick={handleResetClick}>
+          Reset
+        </button>
+
+        <input
+          type="Number"
+          value={amount}
+          placeholder="Enter the amount"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+
+        <button aria-label="Increment by number" onClick={handleIncrementNumber}>
+          Increment by number 
         </button>
       </div>
     </div>
-  )
+  );
 }
--- redux-toolkit/src/features/counter/counterSlice.jsx
+++ redux-toolkit/src/features/counter/counterSlice.jsx
@@ -5,6 +5,8 @@ const initialState = {
 }
 
 export const counterSlice = createSlice({
+
+  
   name: 'counter',
   initialState,
   reducers: {
@@ -18,7 +20,7 @@ export const counterSlice = createSlice({
       state.value = 0
     },
     incrementByAmount: (state, action) => {
-      state.value += action.payload
+      state.value += Number(action.payload)
     },
   },
 })
