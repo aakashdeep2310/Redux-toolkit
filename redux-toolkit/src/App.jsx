26308368bf87479771c824af033b8d445fdd0f63
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-12-04 19:51:33 +0530

    This is first commit

---
+++ redux-toolkit/src/App.jsx
@@ -0,0 +1,18 @@
+
+// import Store from "./redux/store"
+import Counter  from "./features/counter/Counter"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Hello Ji
+        <Counter></Counter>
+        {/* <Store></Store> */}
+      </div>
+    </>
+  )
+}
+
+export default App
